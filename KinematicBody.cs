c5a9e7cdedd455dfe5f02ebffdfa8d3203c561df
Kargisa <luca.wolf@outlook.com>
2023-10-19 22:55:48 +0200

    Actions and test GetPhysicsBody

--- KinematicBody.cs
+++ KinematicBody.cs
@@ -8,50 +8,84 @@ public class KinematicBody : MonoBehaviour
 {
     public WeakReference<NEWTONS.Core.KinematicBody> body;
     [SerializeField]
-    private bool useGravity = true;
-    public bool UseGravity 
-    {  
-        get => useGravity; 
-        set 
+    private bool useGravity = false;
+    [SerializeField]
+    private UnityEngine.Vector3 initVelocity;
+
+    public bool UseGravity
+    {
+        get => GetPhysicsBody().UseGravity;
+        set
         {
             if (!body.TryGetTarget(out NEWTONS.Core.KinematicBody b))
                 throw new Exception("No KinematicBody");
             if (b.UseGravity != value)
                 b.UseGravity = value;
-            useGravity = value; 
-        } 
+        }
     }
 
-    [SerializeField]
-    private UnityEngine.Vector3 velocity;
-    public UnityEngine.Vector3 Velocity 
-    { 
-        get => velocity; 
-        set 
+    public UnityEngine.Vector3 Velocity
+    {
+        get => GetPhysicsBody().Velocity.ToUnityVector();
+        set
         {
             if (!body.TryGetTarget(out NEWTONS.Core.KinematicBody b))
                 throw new Exception("No KinematicBody");
             if (b.Velocity != value.ToNewtonsVector())
                 b.Velocity = value.ToNewtonsVector();
-            velocity = value; 
-        } 
+        }
+    }
+
+    public UnityEngine.Vector3 Position
+    {
+        get => GetPhysicsBody().Position.ToUnityVector();
+        set
+        {
+            if (!body.TryGetTarget(out NEWTONS.Core.KinematicBody b))
+                throw new Exception("No KinematicBody");
+            if (b.Position != value.ToNewtonsVector())
+                b.Position = value.ToNewtonsVector();
+        }
     }
 
     private void Awake()
     {
-
-        body = new WeakReference<NEWTONS.Core.KinematicBody>(
-        new NEWTONS.Core.KinematicBody()
-            {
-                Position = transform.position.ToNewtonsVector(),
-                UseGravity = useGravity,
-                Velocity = velocity.ToNewtonsVector()
-            });
+        NEWTONS.Core.KinematicBody physicsBody = new NEWTONS.Core.KinematicBody()
+        {
+            Position = transform.position.ToNewtonsVector(),
+            UseGravity = useGravity,
+            Velocity = initVelocity.ToNewtonsVector(),
+        };
+        physicsBody.UpdatePosition += UpdateTransformPosition;
+        body = new WeakReference<NEWTONS.Core.KinematicBody>(physicsBody);
         PhysicsWorld.tests.Add(this);
+    }
+
+    //Experimental
+    public NEWTONS.Core.KinematicBody GetPhysicsBody()
+    {
+        if (!body.TryGetTarget(out NEWTONS.Core.KinematicBody target))
+        {
+            Debug.LogWarning("No Physics Body Attached to the KinematicBody, KinematicBody will be Destroyed");
+            Destroy(gameObject);
+            return null;
+        }
+        return target;
     }
 
     private void OnDestroy()
     {
         PhysicsWorld.DestroyBody(this);
+    }
+
+    private void UpdateTransformPosition()
+    {
+        transform.position = GetPhysicsBody().Position.ToUnityVector();
+    }
+
+    //Needs own Quaternion implementation
+    private void UpdateTransformRotation()
+    {
+        //transform.rotation = GetPhysicsBody().Rotation;
     }
 }
--- PhysicsWorld.cs
+++ PhysicsWorld.cs
@@ -17,19 +17,6 @@ public class PhysicsWorld : MonoBehaviour
     private void FixedUpdate()
     {
         NEWTONS.Core.Physics.Update(Time.fixedDeltaTime);
-        Debug.Log(NEWTONS.Core.Physics.Bodies.Count);
-        for (int i = 0; i < tests.Count; i++)
-        {
-            if (!tests[i].body.TryGetTarget(out NEWTONS.Core.KinematicBody b))
-                throw new Exception("No KinematicBody");
-            UnityEngine.Vector3 enginePos = b.Position.ToUnityVector();
-            UnityEngine.Vector3 engineVelocity = b.Velocity.ToUnityVector();
-
-            if (tests[i].transform.position != enginePos)
-                tests[i].transform.position = enginePos;
-            if (tests[i].Velocity != engineVelocity)
-                tests[i].Velocity = engineVelocity;
-        }
     }
 
     public static void DestroyBody(KinematicBody test)
