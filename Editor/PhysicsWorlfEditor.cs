56cbfc0a5aecc4c15d9da95168117b424d47140d
Kargisa <luca.wolf@outlook.com>
2023-10-25 18:06:23 +0200

    PhysicsWorld Editor + initial values

---
+++ Editor/PhysicsWorlfEditor.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(PhysicsWorld))]
+public class PhysicsWorlfEditor : Editor
+{
+    PhysicsWorld physicsWorld;
+
+    private void OnEnable()
+    {
+        physicsWorld = (PhysicsWorld)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        DrawProps();
+    }
+
+    private void DrawProps()
+    {
+        if (Application.isPlaying)
+        {
+            PhysicsWorld.Gravity = EditorGUILayout.Vector3Field("Gravity", PhysicsWorld.Gravity);
+            PhysicsWorld.UseCustomDrag = EditorGUILayout.Toggle("Use Physical Drag", PhysicsWorld.UseCustomDrag);
+            if (!PhysicsWorld.UseCustomDrag)
+                return;
+            PhysicsWorld.Temperature = Mathf.Max(EditorGUILayout.FloatField("Temperature", PhysicsWorld.Temperature), NEWTONS.Core.PhysicsInfo.MinTemperature);
+            PhysicsWorld.Density = Mathf.Max(EditorGUILayout.FloatField("Density", PhysicsWorld.Density), NEWTONS.Core.PhysicsInfo.MinDensity);
+        }
+        else
+        {
+            physicsWorld.initialGravity = EditorGUILayout.Vector3Field("Gravity", physicsWorld.initialGravity);
+            physicsWorld.initialUseCustomDrag = EditorGUILayout.Toggle("Use Physical Drag", physicsWorld.initialUseCustomDrag);
+            if (!physicsWorld.initialUseCustomDrag)
+                return;
+            physicsWorld.initialTemperature = Mathf.Max(EditorGUILayout.FloatField("Temperature", physicsWorld.initialTemperature), NEWTONS.Core.PhysicsInfo.MinTemperature);
+            physicsWorld.initialDensity = Mathf.Max(EditorGUILayout.FloatField("Density", physicsWorld.initialDensity), NEWTONS.Core.PhysicsInfo.MinDensity);
+        }
+    }
+}
--- PhysicsWorld.cs
+++ PhysicsWorld.cs
@@ -1,14 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using NEWTONS.Core;
-using System;
 
 public class PhysicsWorld : MonoBehaviour
 {
     public static List<KinematicBody> tests = new List<KinematicBody>();
 
-    public UnityEngine.Vector3 Gravity
+    /// <summary>
+    /// Internal NEWTONS field. Do not use.
+    /// </summary>
+    /// <remarks>
+    /// Do not use this method directly. Insteade use <see cref="Temperature"/> to alter the Temperature.
+    /// </remarks>
+    public float initialTemperature = NEWTONS.Core.Physics.Temperature;
+    /// <summary>
+    /// Internal NEWTONS field. Do not use.
+    /// </summary>
+    /// <remarks>
+    /// Do not use this method directly. Insteade use <see cref="Density"/> to alter the Density.
+    /// </remarks>
+    public float initialDensity = NEWTONS.Core.Physics.Density;
+    /// <summary>
+    /// Internal NEWTONS field. Do not use.
+    /// </summary>
+    /// <remarks>
+    /// Do not use this method directly. Insteade use <see cref="UseCustomDrag"/> to alter the UseCustomDrag.
+    /// </remarks>
+    public bool initialUseCustomDrag = NEWTONS.Core.Physics.UseCustomDrag;
+    /// <summary>
+    /// Internal NEWTONS field. Do not use.
+    /// </summary>
+    /// <remarks>
+    /// Do not use this method directly. Insteade use <see cref="Gravity"/> to alter the Gravity.
+    /// </remarks>
+    public Vector3 initialGravity = NEWTONS.Core.Physics.Gravity.ToUnityVector();
+
+    public static bool UseCustomDrag 
+    {
+        get => NEWTONS.Core.Physics.UseCustomDrag;
+        set => NEWTONS.Core.Physics.UseCustomDrag = value;
+    }
+
+    public static float Temperature
+    {
+        get => NEWTONS.Core.Physics.Temperature;
+        set { NEWTONS.Core.Physics.Temperature = value; }
+    }
+
+    public static float Density
+    {
+        get => NEWTONS.Core.Physics.Density;
+        set { NEWTONS.Core.Physics.Density = value; }
+    }
+
+
+    public static UnityEngine.Vector3 Gravity
     {
         get => NEWTONS.Core.Physics.Gravity.ToUnityVector();
         set { NEWTONS.Core.Physics.Gravity = value.ToNewtonsVector(); }
