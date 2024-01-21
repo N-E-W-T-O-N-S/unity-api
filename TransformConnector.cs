16105492e56b7fd6ff67d07f8e2522f639bbfdaa
Kargisa <personal@kargisa.com>
2024-01-21 19:34:38 +0100

    mini changes

--- TransformConnector.cs
+++ TransformConnector.cs
@@ -27,6 +27,7 @@ public class TransformConnector : MonoBehaviour
         if (transform.position != _oldPosition)
         {
             OnPositionChanged?.Invoke();
+            Debug.Log("HELLO");
             _oldPosition = transform.position;
         }
         if (transform.rotation != _oldRotation)
